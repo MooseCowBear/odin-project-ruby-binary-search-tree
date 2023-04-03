52d1b0bee3527873b5fff168908e7624a6785e33
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-04-03 11:04:19 -0400

    Add build tree method

--- bst.rb
+++ bst.rb
@@ -14,4 +14,30 @@ class Node
   end
 end
 
+class Tree
+  def initialize(arr)
+    @root = build_tree(arr)
+  end
 
+  def build_tree(arr)
+    arr = arr.uniq.sort
+    return nil if arr.length == 0
+
+    mid = arr.length / 2
+    root = Node.new(arr[mid])
+
+    root.left = build_tree(arr[...mid])
+    root.right = build_tree(arr[mid + 1...])
+
+    return root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
+
+test = Tree.new([1, 2, 3, 4, 5, 6])
+test.pretty_print
